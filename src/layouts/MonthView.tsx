50039fff074489db1188bde7857680bd0c2c4f37
Bruce Harrison <bharrison@primetsr.com>
2022-01-25 14:15:18 -0600

    init repo

---
+++ src/layouts/MonthView.tsx
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import React from 'react';
+
+export interface ButtonProps {
+  label: string;
+}
+
+const Button = (props: ButtonProps) => {
+  return <button>{props.label}</button>;
+};
+
+export default Button;
