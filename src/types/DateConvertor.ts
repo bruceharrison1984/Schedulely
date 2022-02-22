c41da4f7ad4fdca5526a9a9a5dd6af1434066216
Bruce Harrison <bharrison@primetsr.com>
2022-02-22 15:52:06 -0600

    alphabetize interface

--- src/types/DateConvertor.ts
+++ src/types/DateConvertor.ts
@@ -5,23 +5,25 @@ export interface DateConvertor {
   /** Add the specified number of months to the date. Using a negative value will subtract that amount. */
   addMonthsToDate: (date: Date, amount: number) => Date;
 
-  /** Compare two dates, returns true if they are in the same month, in the same year */
-  isSameMonth: (firstDate: Date, secondDate: Date) => boolean;
-
   /** Returns all days in the month, split apart by week. Includes leading/trailing days. */
   getCalendarView: (date: Date) => Date[][];
+  /** Get the day number component for a given date */
+  getDayNumber: (date: Date) => number;
 
   /** Get full names of all days of the week */
   getDaysOfWeek: (displaySize: DisplaySize) => string[];
+
+  /** Get the day of week grid index for the end of the event. Used for positioning within the Week css-grid. */
+  getGridEndIndex: (eventEndDate: Date, endOfWeek: Date) => number;
+
+  /** Get the day of week index for the start of the event. Used for positioning within the Week css-grid */
+  getGridStartIndex: (eventDate: Date, startOfWeek: Date) => number;
 
   /** Get the full name of the month for a given date */
   getMonthName: (date: Date) => string;
 
   /** Get the year component for a given date */
   getYear: (date: Date) => number;
-
-  /** Get the day number component for a given date */
-  getDayNumber: (date: Date) => number;
 
   /** Returns true if the date represent today */
   isDateToday: (date: Date) => boolean;
@@ -33,11 +35,8 @@ export interface DateConvertor {
     week: Date[]
   ) => boolean;
 
-  /** Get the day of week grid index for the end of the event. Used for positioning within the Week css-grid. */
-  getGridEndIndex: (eventEndDate: Date, endOfWeek: Date) => number;
-
-  /** Get the day of week index for the start of the event. Used for positioning within the Week css-grid */
-  getGridStartIndex: (eventDate: Date, startOfWeek: Date) => number;
+  /** Compare two dates, returns true if they are in the same month, in the same year */
+  isSameMonth: (firstDate: Date, secondDate: Date) => boolean;
 }
 
 export enum DisplaySize {
