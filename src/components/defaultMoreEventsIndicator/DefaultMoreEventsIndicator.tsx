d513fe07acdd033be151f387cad5e4f63beb767b
Bruce Harrison <bharrison@primetsr.com>
2022-03-28 11:32:40 -0500

    clean up more events css slightly

--- src/components/defaultMoreEventsIndicator/DefaultMoreEventsIndicator.tsx
+++ src/components/defaultMoreEventsIndicator/DefaultMoreEventsIndicator.tsx
@@ -10,7 +10,7 @@ export const DefaultMoreEventsIndicator: MoreEventsIndicatorComponent = ({
         title={`(${events.length}) total events`}
         onClick={() => console.log(events)}
       >
-        . . .
+        ...
       </div>
     </div>
   );
