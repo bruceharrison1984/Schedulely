b9c650874073d6e6db9119b36514d5625aee82b8
Bruce Harrison <bharrison@primetsr.com>
2022-04-06 10:43:16 -0500

    add docs

--- src/types/components/HeaderComponent.ts
+++ src/types/components/HeaderComponent.ts
@@ -6,10 +6,20 @@ export interface HeaderProps {
   month: string;
   /** The current year the calendar is displaying */
   year: number;
+
+  /** True if the selected month is the same as the current month */
   isCurrentMonth: boolean;
+
+  /** Triggers moving forward one month */
   onNextMonth: () => void;
+
+  /** Triggers moving forward one year */
   onNextYear: () => void;
+
+  /** Triggers moving back one month */
   onPrevMonth: () => void;
+
+  /** Triggers moving back one year */
   onPrevYear: () => void;
 }
 
