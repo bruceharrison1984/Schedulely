f16faa781fafe21db8b32fb32de03c6de427216f
Bruce Harrison <leeharrison1984@gmail.com>
2022-09-29 10:17:58 -0500

    got docusaurus working

--- apps/docs/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import 'schedulely/dist/index.css';
-
-import { AppProps } from 'next/app';
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
---
+++ apps/react-pico-calendar-docs/src/pages/index.tsx
@@ -0,0 +1,46 @@
+import { Schedulely } from 'schedulely';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import React from 'react';
+import clsx from 'clsx';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+import 'schedulely/dist/index.css';
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <HomepageHeader />
+      <main>
+        <Schedulely events={[]} />
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
