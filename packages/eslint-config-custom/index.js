7aa089d0f8dcd071fc53abb021d0838813e378bb
Bruce Harrison <leeharrison1984@gmail.com>
2022-10-03 14:08:59 -0500

    fix linter

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,8 +1,8 @@
 module.exports = {
-  extends: ['next', 'turbo', 'prettier'],
-  plugins: ['sort-imports-es6-autofix'],
+  parser: '@typescript-eslint/parser',
+  extends: ['turbo', 'prettier'],
+  plugins: ['sort-imports-es6-autofix', '@typescript-eslint'],
   rules: {
-    '@next/next/no-html-link-for-pages': 'off',
     'react/jsx-key': 'off',
     'sort-imports-es6-autofix/sort-imports-es6': [
       2,
