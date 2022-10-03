99593e450bcaac22ac757144d100c0efcab2c193
Bruce Harrison <leeharrison1984@gmail.com>
2022-10-03 17:00:21 -0500

    try to get darkmode working in a better way

--- packages/Schedulely/src/Schedulely.tsx
+++ packages/Schedulely/src/Schedulely.tsx
@@ -6,8 +6,9 @@ import {
   ComponentProvider,
 } from '@/providers/index';
 import { DayOfWeekLayout, HeaderLayout, MonthLayout } from '@/layouts/index';
-import { SchedulelyProps } from '@/types/index';
+import { DisplaySize, SchedulelyProps } from '@/types/index';
 import { createDefaultAdapter } from './dateAdapters';
+import { useState, useCallback, useEffect } from 'react';
 
 /**
  * Create an instance of Schedulely
@@ -26,12 +27,34 @@ export const Schedulely = ({
 }: SchedulelyProps) => {
   if (!dateAdapter) throw new Error('Date Adapter must be supplied!');
 
+  const [prefersDarkMode, setPrefersDarkMode] = useState<boolean | undefined>();
+  const media = window.matchMedia('(prefers-color-scheme: dark)');
+
+  const createQueryListener = useCallback(
+    (mediaQuery: string) => {
+      const media = window.matchMedia(mediaQuery);
+      const listener = () =>
+        setPrefersDarkMode(media.matches ? true : undefined);
+      window.addEventListener('change', listener);
+      return listener;
+    },
+    [setPrefersDarkMode]
+  );
+
+  useEffect(() => {
+    var colorWatcher = createQueryListener('(prefers-color-scheme: dark)');
+
+    () => {
+      window.removeEventListener('change', colorWatcher);
+    };
+  }, []);
+
   return (
     <div
       id="schedulely"
       className={[...additionalClassNames, 'schedulely'].join(' ')}
       data-theme={theme}
-      data-dark={dark ? '' : undefined}
+      data-dark={prefersDarkMode}
     >
       <ActionProvider actions={actions}>
         <ComponentProvider calendarComponents={schedulelyComponents}>
