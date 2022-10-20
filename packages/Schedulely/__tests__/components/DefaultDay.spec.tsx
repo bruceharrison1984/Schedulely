3c6227a91ad211e592216a2d35b0443c38f2ac89
Bruce Harrison <leeharrison1984@gmail.com>
2022-10-19 21:06:03 -0500

    add more tests

--- packages/Schedulely/__tests__/components/DefaultDay.spec.tsx
+++ packages/Schedulely/__tests__/components/DefaultDay.spec.tsx
@@ -1,6 +1,6 @@
 import { DayComponentProps } from '@/types';
 import { DefaultDay } from '@/components';
-import { render, screen } from '@testing-library/react';
+import { RenderResult, render } from '@testing-library/react';
 import chance from 'chance';
 
 describe('DefaultDay', () => {
@@ -32,16 +32,71 @@ describe('DefaultDay', () => {
   });
 
   describe('isOverflowed', () => {
-    it('displays indicator if overflowed', () => {
-      const testObject = render(<DefaultDay {...defaults} />);
-      expect(testObject.getByRole('note')).toBeVisible();
+    describe('is true', () => {
+      let testObject: RenderResult;
+
+      beforeEach(() => {
+        testObject = render(<DefaultDay {...defaults} />);
+      });
+
+      it('renders indicator', () => {
+        expect(testObject.getByRole('note')).toBeVisible();
+      });
+
+      it('indicator has help text', () => {
+        expect(testObject.getByRole('note').title).not.toBeNull();
+      });
     });
 
-    it('indicator not rendered if not overflowed', () => {
-      const testObject = render(
-        <DefaultDay {...defaults} isOverflowed={false} />
-      );
-      expect(testObject.queryByRole('note')).toBeNull();
+    describe('is false', () => {
+      let testObject: RenderResult;
+
+      beforeEach(() => {
+        testObject = render(<DefaultDay {...defaults} isOverflowed={false} />);
+      });
+
+      it('indicator not rendered', () =>
+        expect(testObject.queryByRole('note')).toBeNull());
+    });
+  });
+
+  describe('isToday', () => {
+    describe('is true', () => {
+      let testObject: RenderResult;
+
+      beforeEach(() => {
+        testObject = render(<DefaultDay {...defaults} />);
+      });
+
+      it('displays indicator', () => {
+        expect(
+          testObject.getByRole('heading').querySelector('div')?.className
+        ).toContain('default-day-header--indicator');
+      });
+
+      it('renders date number', () => {
+        expect(
+          testObject.getByRole('heading').querySelector('div span')?.textContent
+        ).toEqual(defaults.dateNumber.toString());
+      });
+    });
+
+    describe('is false', () => {
+      let testObject: RenderResult;
+
+      beforeEach(() => {
+        testObject = render(<DefaultDay {...defaults} isToday={false} />);
+      });
+
+      it('indicator not rendered if false', () =>
+        expect(
+          testObject.getByRole('heading').querySelector('div')
+        ).toBeNull());
+
+      it('renders date number', () =>
+        expect(
+          testObject.getByRole('heading').querySelector('span')?.textContent
+        ).toEqual(defaults.dateNumber.toString()));
     });
   });
 });
