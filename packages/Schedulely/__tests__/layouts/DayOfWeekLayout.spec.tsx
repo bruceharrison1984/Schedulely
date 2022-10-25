69497dd986329295a105a2e5de349515fcac85fa
Bruce Harrison <leeharrison1984@gmail.com>
2022-10-25 13:21:51 -0500

    add tests for dayofweeklayout

--- packages/Schedulely/__tests__/layouts/DayOfWeekLayout.spec.tsx
+++ packages/Schedulely/__tests__/layouts/DayOfWeekLayout.spec.tsx
@@ -1,8 +1,8 @@
-import { BreakpointProvider, CalendarProvider } from '@/providers';
+import { BreakpointProvider } from '@/providers';
 import { CalendarState, ComponentSize } from '@/types';
 import { DayOfWeekLayout } from '@/layouts';
+import { RenderResult, render } from '@testing-library/react';
 import { createDefaultAdapter } from '@/dateAdapters';
-import { render, within } from '@testing-library/react';
 import { useCalendar } from '../../src/hooks/useCalendar';
 import React from 'react';
 
@@ -22,7 +22,7 @@ const daysOfWeek = [
 const hook: CalendarState = {
   currentMonth: new Date(),
   dateAdapter: createDefaultAdapter(),
-  getDaysOfWeek: (componentSize: ComponentSize) => daysOfWeek,
+  getDaysOfWeek: () => daysOfWeek,
   onNextMonth: () => null,
   onPrevMonth: () => null,
   onNextYear: () => null,
@@ -30,16 +30,19 @@ const hook: CalendarState = {
   calendarWithEvents: [],
 };
 
-xdescribe('DayOfWeekLayout', () => {
+describe('DayOfWeekLayout', () => {
   mockUseCalendar.mockReturnValue(hook);
+  let testObject: RenderResult;
 
-  const testObject = render(
-    <BreakpointProvider containerRef={React.createRef()}>
-      <DayOfWeekLayout></DayOfWeekLayout>
-    </BreakpointProvider>
-  );
+  beforeEach(() => {
+    testObject = render(
+      <BreakpointProvider containerRef={React.createRef()}>
+        <DayOfWeekLayout />
+      </BreakpointProvider>
+    );
+  });
 
   test.each(daysOfWeek)('%s is rendered', (value) => {
-    expect(testObject.findByText(value)).toBeInTheDocument();
+    expect(testObject.queryByText(value)).not.toBeNull();
   });
 });
