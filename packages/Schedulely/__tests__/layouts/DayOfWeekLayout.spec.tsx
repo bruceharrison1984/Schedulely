25c679707a7fdc68c063da1387fb21b4bcf5e556
Bruce Harrison <leeharrison1984@gmail.com>
2022-10-26 16:18:03 -0500

    simplify test

--- packages/Schedulely/__tests__/layouts/DayOfWeekLayout.spec.tsx
+++ packages/Schedulely/__tests__/layouts/DayOfWeekLayout.spec.tsx
@@ -1,38 +1,37 @@
-import { BreakpointProvider } from '@/providers';
+import {
+  ActionProvider,
+  BreakpointProvider,
+  CalendarProvider,
+} from '@/providers';
 import { DayOfWeekLayout } from '@/layouts';
 import { RenderResult, render } from '@testing-library/react';
-import { getCalendarProviderProps } from '../testHelpers/component.testHelper';
-import { useCalendar } from '../../src/hooks/useCalendar';
+import { createDefaultAdapter } from '@/dateAdapters';
 import React from 'react';
 
-jest.mock('../../src/hooks/useCalendar');
-const mockUseCalendar = useCalendar as jest.MockedFunction<typeof useCalendar>;
-
-const daysOfWeek = [
-  'Sunday',
-  'Monday',
-  'Tueday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-];
-
-const hook = getCalendarProviderProps({ daysOfWeek: daysOfWeek });
+const smallDaysOfWeek = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
 describe('DayOfWeekLayout', () => {
-  mockUseCalendar.mockReturnValue(hook);
   let testObject: RenderResult;
 
   beforeEach(() => {
     testObject = render(
       <BreakpointProvider containerRef={React.createRef()}>
-        <DayOfWeekLayout />
+        <ActionProvider actions={{ onMonthChangeClick: () => null }}>
+          <CalendarProvider
+            dateAdapter={createDefaultAdapter('en-us')}
+            initialDate={new Date().toISOString()}
+            calendarEvents={[]}
+          >
+            <DayOfWeekLayout />
+          </CalendarProvider>
+        </ActionProvider>
       </BreakpointProvider>
     );
   });
 
-  test.each(daysOfWeek)('%s is rendered', (value) => {
-    expect(testObject.queryByText(value)).not.toBeNull();
+  describe('small display header text', () => {
+    test.each(smallDaysOfWeek)('%s is rendered', (value) => {
+      expect(testObject.queryAllByText(value)).not.toBeNull();
+    });
   });
 });
