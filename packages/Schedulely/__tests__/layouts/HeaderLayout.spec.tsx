3cebd6ed4dce42c9e3bbd0db14bf98d2026eb46d
Bruce Harrison <leeharrison1984@gmail.com>
2022-10-28 12:16:56 -0500

    add more tests to header

--- packages/Schedulely/__tests__/layouts/HeaderLayout.spec.tsx
+++ packages/Schedulely/__tests__/layouts/HeaderLayout.spec.tsx
@@ -1,21 +1,63 @@
+import { Chance } from 'chance';
+import { DefaultHeader } from '@/components';
 import { HeaderComponent } from '@/types';
 import { HeaderLayout } from '@/layouts';
-import { render } from '@testing-library/react';
+import { RenderResult, render } from '@testing-library/react';
 
-const mockHeaderComponent: HeaderComponent = () => <div>HEADER COMPONENT</div>;
+let mockHeaderComponent: HeaderComponent = DefaultHeader;
+const mockCurrentMonth = Chance().month();
+const mockCurrentYear = Chance().year();
+const mockIsCurrentMonth = Chance().bool();
 
 jest.mock('@/hooks', () => ({
   useComponents: jest.fn(() => ({
     headerComponent: mockHeaderComponent,
   })),
-  useCalendar: jest.fn(() => ({})),
-  useBreakpoint: jest.fn(() => ({})),
+  useCalendar: jest.fn(() => ({
+    currentMonth: mockCurrentMonth,
+    currentYear: mockCurrentYear,
+    isCurrentMonth: mockIsCurrentMonth,
+  })),
+  useBreakpoint: jest.fn(() => ({
+    breakpoint: 'small',
+  })),
 }));
 
 describe('HeaderLayout', () => {
-  const testObject = render(<HeaderLayout />);
+  describe('with DefaultHeader component', () => {
+    let testObject: RenderResult;
 
-  it('uses custom header component', () => {
-    expect(testObject.getByText('HEADER COMPONENT')).toBeTruthy();
+    beforeEach(() => {
+      testObject = render(<HeaderLayout />);
+    });
+
+    it('passes currentMonth value', () =>
+      expect(
+        testObject.getByText(mockCurrentMonth, { exact: false })
+      ).toBeTruthy());
+
+    it('passes currentYear value', () =>
+      expect(
+        testObject.getByText(mockCurrentYear, { exact: false })
+      ).toBeTruthy());
+
+    it('passes isCurrentMonth', () => {
+      const result = expect(testObject.queryByRole('alert'));
+      mockIsCurrentMonth ? result.toBeTruthy() : result.toBeFalsy();
+    });
+  });
+
+  describe('with custom header component', () => {
+    let testObject: RenderResult;
+    let mockHeaderText: string;
+
+    beforeEach(() => {
+      mockHeaderText = Chance().string();
+      mockHeaderComponent = () => <div>{mockHeaderText}</div>;
+      testObject = render(<HeaderLayout />);
+    });
+
+    it('uses custom header component', () =>
+      expect(testObject.getByText(mockHeaderText)).toBeTruthy());
   });
 });
