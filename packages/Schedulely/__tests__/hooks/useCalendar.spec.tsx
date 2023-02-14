512a7d3daa4b823a4109acb52affb1978a688a09
Bruce Harrison <leeharrison1984@gmail.com>
2023-02-14 12:31:16 -0600

    setup test harness to useCalendar hook

--- packages/Schedulely/__tests__/hooks/useCalendar.spec.tsx
+++ packages/Schedulely/__tests__/hooks/useCalendar.spec.tsx
@@ -1,7 +1,42 @@
+import { CalendarProvider } from '@/providers';
+import { ComponentSize } from '@/types';
+import { ReactNode } from 'react';
+import { createDefaultAdapter } from '@/dateAdapters';
 import { render } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
 import { useCalendar } from '@/hooks';
 
+const mockOnMonthChangeClick = jest.fn();
+jest.mock('@/hooks/useActions', () => ({
+  useActions: jest.fn(() => ({
+    onMonthChangeClick: mockOnMonthChangeClick,
+  })),
+}));
+
+const mockBreakpoint: ComponentSize = 'large';
+jest.mock('@/hooks/useBreakpoint', () => ({
+  useBreakpoint: jest.fn(() => ({
+    breakpoint: mockBreakpoint,
+  })),
+}));
+
+const wrapper = ({ children }: { children: ReactNode }) => (
+  <CalendarProvider
+    dateAdapter={createDefaultAdapter()}
+    initialDate={new Date(2023, 1, 14).toISOString()}
+    calendarEvents={[]}
+  >
+    {children}
+  </CalendarProvider>
+);
+
 describe('useCalendar', () => {
+  it('currentDate should return correct date', () => {
+    const { result } = renderHook(() => useCalendar(), { wrapper });
+
+    expect(result.current.currentDate).toEqual(new Date(2023, 1, 14));
+  });
+
   it('throws when called outside of provider', () => {
     const ExceptionWrapper = () => {
       expect(useCalendar).toThrowError(/must be used within/);
