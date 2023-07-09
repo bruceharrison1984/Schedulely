1150ae5c4d309580a35920182a9b0946f004e1ce
Bruce Harrison <leeharrison1984@gmail.com>
2023-07-09 00:03:52 -0500

    add coverage report

--- packages/Schedulely/vitest.config.ts
+++ packages/Schedulely/vitest.config.ts
@@ -3,6 +3,9 @@ import path from 'path';
 
 export default defineConfig({
   test: {
+    coverage: {
+      reporter: ['text', 'json', 'json-summary'],
+    },
     globals: true,
     environment: 'jsdom',
     include: ['**/__tests__/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
