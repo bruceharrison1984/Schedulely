6517686c7192a8dc916efa34f5d5fe9b51bd11c8
Bruce Harrison <leeharrison1984@gmail.com>
2023-07-24 13:40:24 -0500

    fix component issue

--- apps/schedulely-docs/components/pre.tsx
+++ apps/schedulely-docs/components/pre.tsx
@@ -7,21 +7,15 @@ import ReactDOMServer from 'react-dom/server';
 import he from 'he';
 
 export const LivePre = (props: PropsWithChildren) => {
-  const options = props.children.props.className.split(',');
+  const children = props.children as JSX.Element;
+  const options = children.props.className.split(',');
   const language = options[0].replace(/language-/, '');
   const live = !!options[1];
   const code = he.decode(
-    ReactDOMServer.renderToString(props.children.props.children)
+    ReactDOMServer.renderToString(children.props.children)
   );
 
-  if (
-    live &&
-    isValidElement(props.children) &&
-    props.children.type.name === 'Code'
-  ) {
-    // const code = he.decode(
-    //   ReactDOMServer.renderToString(props.children.props.children)
-    // );
+  if (live && isValidElement(props.children) && children.type.name === 'Code') {
     return (
       <LiveProvider
         code={code}
