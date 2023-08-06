f2ff8de552eee910bfaa72e625e3993ebeb34e78
Scott Lovegrove <scott@ferretlabs.com>
2023-08-06 17:06:27 +0100

    test: Fixes and adds tests for custom keyboard shortcuts

--- packages/Schedulely/__tests__/hooks/useKeyboardControls.spec.tsx
+++ packages/Schedulely/__tests__/hooks/useKeyboardControls.spec.tsx
@@ -1,3 +1,4 @@
+import { ActionProvider } from '../../src/providers';
 import { RenderResult, fireEvent, render } from '@testing-library/react';
 import { useKeyboardControls } from '@/hooks/useKeyboardControls';
 import { vi } from 'vitest';
@@ -15,9 +16,22 @@ vi.mock('@/hooks/useCalendar', () => ({
   })),
 }));
 
-const TestWrapper = () => {
+const mockOnUpArrow = vi.fn();
+const mockOnDownArrow = vi.fn();
+const mockOnLeftArrow = vi.fn();
+const mockOnRightArrow = vi.fn();
+
+const Wrapper = () => {
   useKeyboardControls();
   return <div></div>;
+};
+
+const TestWrapper = () => {
+  return (
+    <ActionProvider>
+      <Wrapper />
+    </ActionProvider>
+  );
 };
 
 describe('useKeyboardControls', () => {
@@ -45,5 +59,40 @@ describe('useKeyboardControls', () => {
   it('ArrowDown calls onPrevYear', () => {
     fireEvent.keyDown(testObject.container, { key: 'ArrowDown' });
     expect(mockOnPrevYear).toHaveBeenCalledTimes(1);
+  });
+
+  describe('custom keyboard events', () => {
+    beforeEach(() => {
+      vi.mock('@/hooks/useActions', () => ({
+        useActions: vi.fn(() => ({
+          keyboardEvents: {
+            onUpArrow: mockOnUpArrow,
+            onDownArrow: mockOnDownArrow,
+            onLeftArrow: mockOnLeftArrow,
+            onRightArrow: mockOnRightArrow,
+          },
+        })),
+      }));
+    });
+
+    it('ArrowUp calls onUpArrow', () => {
+      fireEvent.keyDown(testObject.container, { key: 'ArrowUp' });
+      expect(mockOnUpArrow).toHaveBeenCalledTimes(1);
+    });
+
+    it('ArrowRight calls onRightArrow', () => {
+      fireEvent.keyDown(testObject.container, { key: 'ArrowRight' });
+      expect(mockOnRightArrow).toHaveBeenCalledTimes(1);
+    });
+
+    it('ArrowLeft calls onLeftArrow', () => {
+      fireEvent.keyDown(testObject.container, { key: 'ArrowLeft' });
+      expect(mockOnLeftArrow).toHaveBeenCalledTimes(1);
+    });
+
+    it('ArrowDown calls onDownArrow', () => {
+      fireEvent.keyDown(testObject.container, { key: 'ArrowDown' });
+      expect(mockOnDownArrow).toHaveBeenCalledTimes(1);
+    });
   });
 });
